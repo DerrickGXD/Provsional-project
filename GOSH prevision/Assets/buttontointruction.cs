c3cf7a4725a7378a8c80074925116e45ef33b49d
sifangdu <dusifang@outlook.com>
2018-10-27 16:52:15 +0100

    helppage unfinished

---
+++ GOSH prevision/Assets/buttontointruction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttontointruction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        bshjkcbasbchdlsbcjdbjdbcjsdl`cbjdskacvhdlsuvcuslcvdsajvchdjvchsadkvxjs
+        vsnmdcbhbchjslbJCKBNDKLVNA DSHJCBDKSAJBCHKSBCAL
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
