a51e957c81fbfdefdf69ece3adeb80856fd0dc2f
Ali Zia <zcabzia@ucl.ac.uk>
2018-10-29 12:54:18 +0000

    color changed

---
+++ GOSH prevision/Assets/ChangeColour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColour : MonoBehaviour {
+
+    public Material matofObject;
+    public Color new_color;
+    public KeyCode changecol;
+
+	// Use this for initialization
+	void Start () {
+
+        matofObject.color = Color.red;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        matofObject.color = Color.red;
+    }
+}
